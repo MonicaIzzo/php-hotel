ebd7195d29267bc19ff8b3c672a347abaeb88832
MonicaIzzo <info@monicaizzo.net>
2023-07-23 07:43:33 +0200

    ho importato l'array degli hotel

--- index.php
+++ index.php
@@ -1,20 +1,26 @@
 <!--
 Esercizio di oggi: PHP Hotel
 nome repo: php-hotel
+
 Descrizione
 Partiamo da questo array di hotel. https://www.codepile.net/pile/OEWY7Q1G
 Copiatelo su un vostro file per usarlo nell'esercizio
 Prima stampate in pagina i dati, senza preoccuparvi dello stile ed effettuate un push con questo risultato.
 Solo dopo aggiungete Bootstrap e mostrate le informazioni all'interno di una tabella.
-Non è necessario spezzettare l'esercizio coi partials ma se ci riuscite tanto meglio :occhiolino:
+Non è necessario spezzettare l'esercizio coi partials ma se ci riuscite tanto meglio 
+
 Bonus:
 1 - Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio.
+
 2 - Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore)
 NOTA :mega:: deve essere possibile utilizzare entrambi i filtri contemporaneamente (es. ottenere una lista con hotel che dispongono di parcheggio e che hanno un voto di tre stelle o superiore)
 Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gli hotel.
-Buon lavoro e buon divertimento!
 
 -->
+
+<?php
+require 'partials/data/hotel.php';
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -36,6 +42,21 @@ Buon lavoro e buon divertimento!
 </head>
 
 <body>
+    <div class="container mt-5">
+        <!-- HEADER -->
+        <header>
+            <h1>Hotels</h1>
+        </header>
+        <!-- MAIN -->
+        <main>
+            <ul>
+                <?php foreach ($hotels as $hotel) : ?>
+                    <li><?= $hotel['name'] ?></li>
+                <?php endforeach ?>
+            </ul>
+        </main>
+
+    </div>
 </body>
 
 </html>
